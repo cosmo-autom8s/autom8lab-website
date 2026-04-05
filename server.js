6d5a7d86e7f3fac1643c89a6b8134f05e087b024
Cosmo Cosmin Lungu <cosmoai88@gmail.com>
2026-04-06 01:06:29 +0300

    feat: initialize Node.js project scaffold with Express + EJS

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Autom8Lab dev server running at http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
